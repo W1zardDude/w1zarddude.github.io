64fc42c1bd2897e7f266ab823dbaca675aac6ee4
Kostya <wizarddudecool@gmail.com>
2019-10-03 01:07:04 +0300

    soon

---
+++ js.js
@@ -0,0 +1,23 @@
+var comingdate = new Date("Oct 5, 2019 00:00:00");
+
+var d = document.getElementById('d');
+var h = document.getElementById("h");
+var m = document.getElementById("m");
+var s = document.getElementById("s");
+
+
+var x = setInterval(function(){
+    var now = new Date();
+    var des = comingdate.getTime() -  now.getTime();
+    var days = Math.floor(des/(1000 * 60 * 60 * 24));
+    var hours = Math.floor(des%(1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var mins = Math.floor(des%(1000 * 60 * 60) / (1000 * 60));
+    var secs = Math.floor(des%(1000 * 60) / 1000);
+
+    d.innerHTML = days;
+    h.innerHTML = hours;
+    m.innerHTML = mins;
+    s.innerHTML = secs;
+
+    if(des <= 0) clearInterval(x);
+}, 1000)
