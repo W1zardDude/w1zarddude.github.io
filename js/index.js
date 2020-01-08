f69d53d00e9b555462f91d5cb35cc238e6ecfef4
W1zardDude <44836911+W1zardDude@users.noreply.github.com>
2020-01-08 23:24:30 +0200

    main-content-filtration

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const content = {
+    svytoshino: {
+        rooms: 1,
+        price: 50000,
+        type: "new",
+        developer: "kah"
+    },
+    svytoshino: {
+        rooms: 2,
+        price: 60000,
+        type: "new",
+        developer: "kah"
+    },
+    svytoshino: {
+        rooms: 3,
+        price: 70000,
+        type: "new",
+        developer: "kah"
+    },
+    svytoshino: {
+        rooms: 4,
+        price: 80000,
+        type: "new",
+        developer: "kah"
+    },
+    podol: {
+        rooms: 1,
+        price: 30000,
+        type: "old",
+        developer: "kah"
+    },
+    podol: {
+        rooms: 2,
+        price: 35000,
+        type: "old",
+        developer: ""
+    },
+    podol: {
+        rooms: 3,
+        price: 40000,
+        type: "old",
+        developer: ""
+    },
+    podol: {
+        rooms: 4,
+        price: 45000,
+        type: "old",
+        developer: ""
+    },
+}
