7741c8014ede0d886ee8e6d366408ab033b12c09
W1zardDude <44836911+W1zardDude@users.noreply.github.com>
2020-01-16 16:33:53 +0200

    preloader + some navigation menu fixes

--- js/navigation-menu.js
+++ js/navigation-menu.js
@@ -1,20 +1,37 @@
 const header = document.getElementById('header'),
       slider = document.getElementById('slider'),
       mainContent = document.getElementById('main-content'),
-      buttonMap = document.getElementById('openMap'),
+      buttonMap = Array.from(document.querySelectorAll('.openMap')),
       map = document.getElementById('map'),
-      homeButton = document.getElementById('home');
+      homeButton = Array.from(document.querySelectorAll('.home'));
 
 
-buttonMap.addEventListener('click', function(){
-    header.style.display = 'flex';
-    slider.style.display = 'none';
-    mainContent.style.display = 'none';
-    map.style.display = 'block';
+const toggleMobileMenu = document.querySelector('#toggle'),
+      mobLink = Array.from(document.querySelectorAll('.mobile-menu-link'));
+
+mobLink.forEach(link =>{
+    link.addEventListener('click', function(){
+        toggle.checked = false;
+    })
 })
-homeButton.addEventListener('click', function(){
-    header.style.display = 'flex';
-    slider.style.display = 'block';
-    mainContent.style.display = 'flex';
-    map.style.display = 'none';
+
+
+buttonMap.forEach(button =>{
+    button.addEventListener('click', function(){
+        header.style.display = 'flex';
+        slider.style.display = 'none';
+        mainContent.style.display = 'none';
+        map.style.display = 'block';
+    })
 })
+homeButton.forEach(button => {
+    button.addEventListener('click', function(){
+        header.style.display = 'flex';
+        slider.style.display = 'block';
+        mainContent.style.display = 'none';
+        map.style.display = 'none';
+    })
+    
+})
+
+
